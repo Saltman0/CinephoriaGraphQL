d661748640eedfc57051ec2d113e90cccce05dc4
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-23 17:09:10 +0100

    Resolvers updated

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,6 +4,9 @@ export const resolvers: Resolvers = {
   Query: {
     halls: (_, { cinemaId }, { dataSources }) => {
       return dataSources.infrastructureApi.getHalls(cinemaId);
+    },
+    bookings: (_, { userId }, { dataSources }) => {
+      return dataSources.bookingApi.getBookings(userId);
     }
   },
   Hall: {
@@ -17,6 +20,22 @@ export const resolvers: Resolvers = {
   Showtime: {
     movie: ({ movieId }, _, { dataSources }) => {
       return dataSources.movieApi.getMovie(movieId);
+    },
+    hall: ({ hallId }, _, { dataSources }) => {
+      return dataSources.infrastructureApi.getHall(hallId);
+    }
+  },
+  Booking: {
+    showtime: ({ showtimeId }, _, { dataSources }) => {
+      return dataSources.showtimeApi.getShowtime(showtimeId);
+    },
+    bookingSeats: ({ id }, _, { dataSources }) => {
+      return dataSources.bookingApi.getBookingSeats(id);
+    }
+  },
+  BookingSeat: {
+    seat: ({ seatId }, _, { dataSources }) => {
+      return dataSources.infrastructureApi.getSeat(seatId);
     }
   }
 }
