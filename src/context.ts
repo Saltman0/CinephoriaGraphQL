c291195a59089b429340eb438062aa5ca0acb70e
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-23 17:09:34 +0100

    Update context.ts

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,13 @@
 import { InfrastructureApi } from "./datasources/infrastructure-api.ts";
 import { ShowtimeApi } from "./datasources/showtime-api.ts";
 import { MovieApi } from "./datasources/movie-api.ts";
+import { BookingApi } from "./datasources/booking-api.ts";
 
 export type DataSourceContext = {
   dataSources: {
     infrastructureApi: InfrastructureApi,
     showtimeApi: ShowtimeApi,
-    movieApi: MovieApi
-  };
+    movieApi: MovieApi,
+    bookingApi: BookingApi
+  }
 }
